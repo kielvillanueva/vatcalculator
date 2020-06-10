241311c350f5fb2ee41d25180a0b9c3219ea8571
kielvillanueva <jvillanueva@edukasyon.ph>
2020-06-10 10:14:02 +0800

    vat Calculator inital commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var generator = require('generate-password');
 const express = require('express')
 const app = express()
 var pug = require('pug');
@@ -7,69 +6,129 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'pug');
 
-// npm run devstart
-
-var replace = {'a':'ant', 'A':'ANT', 'b':'bat', 'B':'BAT', 'c':'cat', 'C':'CAT', 'd':'dog', 'D':'DOG', 'e':'egg', 'E':'EGG', 'f':'fox', 'F':'FOX', 'g':'girl', 'G':'GIRL', 'h':'hat', 'H':'HAT', 'i':'ink', 'I':'INK', 'j':'jump', 'J':'JUMP', 'k':'kite', 'K':'KITE', 'l':'lot', 'L':'LOT', 'm':'mom', 'M':'MOM', 'n':'nice', 'N':'NICE', 'o':'old', 'O':'OLD', 'p':'pet', 'P':'PET', 'q':'quiz', 'Q':'QUIZ', 'r':'red', 'R':'RED', 's':'sun', 'S':'SUN', 't':'time', 'T':'TIME', 'u':'up', 'U':'UP', 'v':'van', 'V':'VAN', 'w':'win', 'W':'WIN', 'x':'xray', 'X':'XRAY', 'y':'yoyo', 'Y':'YOYO', 'z':'zinc', 'Z':'ZINC', '0':'Zero', '1':'One', '2':'Two', '3':'Three', '4':'Four', '5':'Five', '6':'Six', '7':'Seven', '8':'Eight', '9':'Nine'}
-
-var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-var numbers = '0123456789';
-var symbols = '!@#$%^&*()+_-=}{[]|\':;"/?.><,`~';
-var upperCase = 'ABCDEFHIJKLMNOPQRSTUVWXYZ';
-
 app.get('/', function (req, res) {
     res.render('index', { title: 'Password Generator', low: true, upp: true, num: true});
 });
 
-function makePassword(pool, length) {
-    var newpass = "";
-  
-    for (var i = 0; i < length; i++)
-      newpass += pool.charAt(Math.floor(Math.random() * pool.length));
-  
-    return newpass;
-}
-
 app.post('/',function(req, res, next){
+    const inputtedData = req.body;
+    console.log(inputtedData);
+    var vatIncMaterialCost = 0, vatIncLaborCost = 0;
+    var orderDiscountMaterialCost = 0, orderDiscountLaborCost = 0;
+    var vatMaterialCost = 0, vatLaborCost = 0;
+    var vatExMaterialCost = 0, vatExLaborCost = 0;
+    var orderDiscountAppliedMaterialCost = 0, orderDiscountAppliedLaborCost = 0;
+    var discountedTotalMaterialCost = 0, discountedTotalLaborCost = 0;
+    var addVatMaterialCost = 0, addVatLaborCost = 0;
+    var ewtLaborCost = 0;
 
-    // var low = '';
-    var remember = '';
-    var pool = '';
+    if (inputtedData['vendorVatStatus'] == 'VAT' && inputtedData['priceVatStatus'] == 'VATInc') {
+        vatIncMaterialCost = 37180.80;
+        vatIncLaborCost = 8000;
 
-    if (req.body.lowerCase) 
-        pool += lowerCase;
-    if (req.body.numbers) 
-        pool += numbers;
-    if (req.body.symbols)
-        pool += symbols;
-    if (req.body.upperCase)
-        pool += upperCase;
-    if (req.body.similar)
-        pool = pool.replace(/[|ilLI`oO0]/g, '');
-    if (req.body.ambiguous)
-        pool = pool.replace(/[{}\[\]\/\\()'"`~,;:.<>]/g, '');
+        if(inputtedData['discountAppliedOn'] == 'VATInc Price') {
+            if(inputtedData['percentOfCostDiscountedOn'] == 'Material Cost' || inputtedData['percentOfCostDiscountedOn'] == 'Total Cost') {
+                orderDiscountMaterialCost = vatIncMaterialCost * inputtedData['percentOfCostDiscount'] / 100;
+            }
+            if (inputtedData['fixedAmountDiscountedOn'] == 'Material Cost') {
+                orderDiscountMaterialCost += parseFloat(inputtedData['fixedAmount']);
+            }
     
+            if(inputtedData['percentOfCostDiscountedOn'] == 'L&E Cost' || inputtedData['percentOfCostDiscountedOn'] == 'Total Cost') {
+                orderDiscountLaborCost = vatIncLaborCost * inputtedData['percentOfCostDiscount'] / 100;
+            }
+            if (inputtedData['fixedAmountDiscountedOn'] == 'L&E Cost') {
+                orderDiscountLaborCost += parseFloat(inputtedData['fixedAmount']);
+            }
+        }
 
-    // var password = generator.generate({
-    //     length: req.body.length,
-    //     numbers: req.body.numbers,
-    //     symbols: req.body.symbols,
-    //     uppercase: req.body.upperCase,
-    //     excludeSimilarCharacters: req.body.similar,
-    //     exclude: amb + low
-    // });
+        vatMaterialCost = (vatIncMaterialCost - orderDiscountMaterialCost) * 0.12;
+        vatLaborCost = (vatIncLaborCost - orderDiscountLaborCost) * 0.12;
 
-    console.log('Pool: ' + pool)
+    } 
 
-    var password = makePassword(pool, req.body.length);
+    if(inputtedData['discountAppliedOn'] == 'VATEx Price') {
+        if(inputtedData['priceVatStatus'] == 'VATInc') {
+            if(inputtedData['percentOfCostDiscountedOn'] == 'Material Cost' || inputtedData['percentOfCostDiscountedOn'] == 'Total Cost') {
+                orderDiscountAppliedMaterialCost = (vatIncMaterialCost - vatMaterialCost) * inputtedData['percentOfCostDiscount'] / 100;
+            }
 
-    console.log('Password: ' + password);
+            if(inputtedData['percentOfCostDiscountedOn'] == 'L&E Cost' || inputtedData['percentOfCostDiscountedOn'] == 'Total Cost') {
+                orderDiscountAppliedLaborCost = (vatIncLaborCost - vatLaborCost) * inputtedData['percentOfCostDiscount'] / 100;
+            }
+        } else {
+            if(inputtedData['percentOfCostDiscountedOn'] == 'Material Cost' || inputtedData['percentOfCostDiscountedOn'] == 'Total Cost') {
+                orderDiscountAppliedMaterialCost = vatExMaterialCost * inputtedData['percentOfCostDiscount'] / 100;
+            }
 
-    remember = password;
+            if(inputtedData['percentOfCostDiscountedOn'] == 'L&E Cost' || inputtedData['percentOfCostDiscountedOn'] == 'Total Cost') {
+                orderDiscountAppliedLaborCost = vatExLaborCost * inputtedData['percentOfCostDiscount'] / 100;
+            }
+        }
 
-    remember = remember.split('').join('  ');
-    remember = remember.replace(/[aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789]/g, m => replace[m]);
+        if (inputtedData['fixedAmountDiscountedOn'] == 'Material Cost') {
+            orderDiscountAppliedMaterialCost += parseFloat(inputtedData['fixedAmount']);
+        }
+        if (inputtedData['fixedAmountDiscountedOn'] == 'L&E Cost') {
+            orderDiscountAppliedLaborCost += parseFloat(inputtedData['fixedAmount']);
+        }
+    } 
 
-    res.render('index', { title: 'Password Generator', pass: password, rem: remember, len: req.body.length, num: req.body.numbers, sym: req.body.symbols, low: req.body.lowerCase, upp: req.body.upperCase, sim: req.body.similar, amb: req.body.ambiguous});
+    if(inputtedData['priceVatStatus'] == 'VATInc') {
+        vatExMaterialCost = vatExMaterialCost - orderDiscountMaterialCost - vatMaterialCost;
+        vatExLaborCost = vatExLaborCost - orderDiscountLaborCost - vatLaborCost;
+
+        discountedTotalMaterialCost = vatIncMaterialCost;
+        discountedTotalLaborCost = vatIncLaborCost;
+    } else {
+        vatExMaterialCost = 37180.80;
+        vatExLaborCost = 8000;
+
+        discountedTotalMaterialCost = vatExMaterialCost;
+        discountedTotalMaterialCost = vatExLaborCost;
+    }
+
+
+    discountedTotalMaterialCost = discountedTotalMaterialCost - orderDiscountMaterialCost - vatMaterialCost - orderDiscountAppliedMaterialCost;
+    discountedTotalLaborCost = discountedTotalLaborCost - orderDiscountLaborCost - vatLaborCost - orderDiscountAppliedLaborCost;
+
+    addVatMaterialCost = discountedTotalMaterialCost * (inputtedData['vendorVatStatus'] == 'VAT' ? 0.12 : 0);
+    addVatLaborCost = discountedTotalLaborCost * (inputtedData['vendorVatStatus'] == 'VAT' ? 0.12 : 0);
+
+    ewtLaborCost = discountedTotalLaborCost * parseFloat(inputtedData['ewtRate']) / 100;
+
+    var netTotalTotalCost = (discountedTotalMaterialCost + discountedTotalLaborCost) + (addVatMaterialCost + addVatLaborCost) - (ewtLaborCost) + 1200;
+
+    let pageData = { 
+        title: 'VAT Calculator', 
+        computedData: true, 
+        vatIncMaterialCost,
+        vatIncLaborCost,
+        vatIncTotalCost: vatIncMaterialCost + vatIncLaborCost,
+        orderDiscountMaterialCost,
+        orderDiscountLaborCost,
+        orderDiscountTotalCost: orderDiscountMaterialCost + orderDiscountLaborCost,
+        vatMaterialCost,
+        vatLaborCost,
+        vatTotalCost: vatMaterialCost + vatLaborCost,
+        vatExMaterialCost,
+        vatExLaborCost,
+        vatExTotalCost: vatExMaterialCost + vatExLaborCost,
+        orderDiscountAppliedMaterialCost,
+        orderDiscountAppliedLaborCost,
+        orderDiscountAppliedTotalCost: orderDiscountAppliedMaterialCost + orderDiscountAppliedLaborCost,
+        discountedTotalMaterialCost,
+        discountedTotalLaborCost,
+        discountedTotalTotalCost: discountedTotalMaterialCost + discountedTotalLaborCost,
+        addVatMaterialCost,
+        addVatLaborCost,
+        addVatTotalCost: addVatMaterialCost + addVatLaborCost,
+        ewtLaborCost,
+        shipmentTotalCost: 1200,
+        netTotalTotalCost, 
+        ...req.body
+    }
+    res.render('index', pageData);
 });
 
 app.listen(3000, () => console.log('listening on port 3000'))
